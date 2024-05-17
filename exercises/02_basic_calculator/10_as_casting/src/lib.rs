d848d6a7c6dbc220dd0d845dd44eedb6609dcedf
Yipeng Sun <syp@umd.edu>
2024-05-18 02:09:09 +0800

    Finish basic calculator.

--- exercises/02_basic_calculator/10_as_casting/src/lib.rs
+++ exercises/02_basic_calculator/10_as_casting/src/lib.rs
@@ -6,17 +6,19 @@ mod tests {
 
     #[test]
     fn u16_to_u32() {
-        assert_eq!(47u16 as u32, todo!());
+        assert_eq!(47u16 as u32, 47u32);
     }
 
     #[test]
     #[allow(overflowing_literals)]
     fn u8_to_i8() {
-        assert_eq!(255 as i8, todo!());
+        assert_eq!(255 as i8, -1i8);
     }
+    // 255
+    // ^1111 1111 -> -128 + 127 = -1
 
     #[test]
     fn bool_to_u8() {
-        assert_eq!(true as u8, todo!());
+        assert_eq!(true as u8, 1u8);
     }
 }
