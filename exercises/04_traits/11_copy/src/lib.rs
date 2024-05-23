00f7bc752cd28bad5dfcb99ea43e88f3dc980801
Yipeng Sun <syp@umd.edu>
2024-05-23 15:44:29 +0800

    DropBomb.

--- exercises/04_traits/11_copy/src/lib.rs
+++ exercises/04_traits/11_copy/src/lib.rs
@@ -1,6 +1,7 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+#[derive(Clone, Copy, Debug)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +9,22 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+use std::ops::Add;
+
+impl Add<Self> for WrappingU32 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self { value: self.value.overflowing_add(rhs.value).0 }
+    }
+}
+
+impl PartialEq for WrappingU32 {
+    fn eq(&self, rhs: &Self) -> bool {
+        self.value == rhs.value
     }
 }
 
