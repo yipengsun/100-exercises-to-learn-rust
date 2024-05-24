b712e715c692c7b66bba4c3e552d568edcde9d8b
Yipeng Sun <syp@umd.edu>
2024-05-24 16:27:05 +0800

    if-let.

--- exercises/05_ticket_v2/04_if_let/src/lib.rs
+++ exercises/05_ticket_v2/04_if_let/src/lib.rs
@@ -8,7 +8,18 @@ impl Shape {
     // TODO: Implement the `radius` method using
     //  either an `if let` or a `let/else`.
     pub fn radius(&self) -> f64 {
-        todo!()
+        // if-let
+        if let Shape::Circle { radius } = &self {
+            *radius
+        } else {
+            panic!()
+        }
+
+        // let-else
+        // let Shape::Circle { radius } = &self else {
+        //     panic!()
+        // };
+        // *radius
     }
 }
 
