c729620c7b605d44fdd48db5368a62cf972ec3a0
Yipeng Sun <syp@umd.edu>
2024-05-28 09:35:35 +0800

    Unwrap and error matching.

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,14 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description, status.clone()) {
+        Ok(t) => t,
+        Err(e) => match e.as_str() {
+            "Description cannot be empty" | "Description cannot be longer than 500 characters" 
+                => Ticket::new(title, "Description not provided".into(), status).unwrap(),
+            _ => panic!("{}", e),
+        } 
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
