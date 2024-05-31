1e04b8dbd11b84f13644711bbb33c2f71a589ff1
Yipeng Sun <syp@umd.edu>
2024-05-31 14:56:20 +0800

    Use thiserror.

--- exercises/05_ticket_v2/12_thiserror/src/lib.rs
+++ exercises/05_ticket_v2/12_thiserror/src/lib.rs
@@ -3,10 +3,20 @@
 //   a `String` field into each variant.
 //   You'll also have to add `thiserror` as a dependency in the `Cargo.toml` file.
 
+use std::error;
+
+#[derive(thiserror::Error, Debug)]
 enum TicketNewError {
+    #[error("Title cannot be empty")]
     TitleCannotBeEmpty,
+
+    #[error("Title cannot be longer than 50 characters")]
     TitleTooLong,
+
+    #[error("Description cannot be empty")]
     DescriptionCannotBeEmpty,
+
+    #[error("Description cannot be longer than 500 characters")]
     DescriptionTooLong,
 }
 
